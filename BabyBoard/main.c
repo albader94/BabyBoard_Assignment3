ef9b4b676d899499085a020a0d7257012984f615
Bader Albader <b.albader94@gmail.com>
2017-10-05 13:37:15 -0600

    main.c
    - organized layout for main.c

--- BabyBoard/main.c
+++ BabyBoard/main.c
@@ -6,7 +6,7 @@
 
 #include <msp430.h>
 #include "uart.h"
-#include "msp430g2553.h”
+
 #include <string.h>
 #include <stdlib.h>
 
@@ -61,55 +61,64 @@ int main(void) {
     if (!strcmp(buffer[0], clock_cmd)) {
       int clock_speed = atoi(buffer[1]);
       switch (clock_speed) {
+
       case 1:
-	uninit_uart();
-	// Set clock speed to 1MHz
-	set_clock(1);
-	// Reinitialize UART with new clock speed, keeping baud rate
-	init_uart(baud);
-	put_str("Set clock speed to 1MHz\n");
-	break;
+        uninit_uart();
+        // Set clock speed to 1MHz
+        set_clock(1);
+        // Reinitialize UART with new clock speed, keeping baud rate
+        init_uart(baud);
+        put_str("Set clock speed to 1MHz\n");
+      break;
+
       case 8:
-	uninit_uart();
-	// Set clock speed to 8MHz
-	set_clock(8);
-	// Reinitialize UART with new clock speed, keeping baud rate
-	init_uart(baud);
-	put_str("Set clock speed to 8MHz\n");
-	break;
+        uninit_uart();
+        // Set clock speed to 8MHz
+        set_clock(8);
+        // Reinitialize UART with new clock speed, keeping baud rate
+        init_uart(baud);
+        put_str("Set clock speed to 8MHz\n");
+      break;
+
       case 16:
-	uninit_uart();
-	// Set clock speed to 16MHz
-	set_clock(16);
-	// Reinitialize UART with new clock speed, keeping baud rate
-	init_uart(baud);
-	put_str("Set clock speed to 16MHz\n");
-	break;
-      default:
-	put_str("Invalid clock speed\n");
-	break;
+        uninit_uart();
+        // Set clock speed to 16MHz
+        set_clock(16);
+        // Reinitialize UART with new clock speed, keeping baud rate
+        init_uart(baud);
+        put_str("Set clock speed to 16MHz\n");
+        break;
+          default:
+        put_str("Invalid clock speed\n");
+	 break;
       }
     }
+
     // BAUD
     else if (!strcmp(buffer[0], baud_cmd)) {
       int baud_rate = atoi(buffer[1]);
       if (!baud_rate) {
-	// If just "baud" is entered
-	put_str("Available baud rates:\n\t[1] 9600\n\t[2] 115200\n");
-      } else if (baud_rate <= 2) {
-	baud = baud_rate - 1; // because indices start from 0
-	put_str("Setting baud rate\n");
-	// Reinitialize UART with new baud rate, keeping clock speed
-	uninit_uart();
-	init_uart(baud);
-      } else {
-	put_str("Invalid baud rate\n");
+        // If just "baud" is entered
+        put_str("Available baud rates:\n\t[1] 9600\n\t[2] 115200\n");
+
+      }
+      else if (baud_rate <= 2) {
+        baud = baud_rate - 1; // because indices start from 0
+        put_str("Setting baud rate\n");
+        // Reinitialize UART with new baud rate, keeping clock speed
+        uninit_uart();
+        init_uart(baud);
+      }
+      else {
+        put_str("Invalid baud rate\n");
       }
     }
+
     // QUOTE
     else if (!strcmp(buffer[0], quote_cmd)) {
       put_str(quote);
     }
+
     // QUIT
     else if (!strcmp(buffer[0], quit_cmd)) {
       put_str("Exiting\n");
@@ -161,11 +170,12 @@ void read_line(char buffer[], unsigned int word_count, unsigned int word_length)
 
     while (!is_whitespace(input, 1)) {
       if (length < word_length - 1) {
-	buffer[count*word_length + length] = input;
-	length += 1;
-      } else {
-	word_too_long = 1;
-	break;
+        buffer[count*word_length + length] = input;
+        length += 1;
+      }
+      else {
+        word_too_long = 1;
+        break;
       }
       input = uart_rx(1);
     }
@@ -177,22 +187,24 @@ void read_line(char buffer[], unsigned int word_count, unsigned int word_length)
 
     if (input == '\n') {
       break;
-    } else {
+    }
+    else {
       count += 1;
       length = 0;
 
       if (count >= word_count) {
-	do {
-	  input = uart_rx(1);
-	} while (is_whitespace(input, 0));
-	if (input == '\n') {
-	  break;
-	}
-	too_many_words = 1;
-	break;
+        do {
+          input = uart_rx(1);
+        } while (is_whitespace(input, 0));
+        if (input == '\n') {
+          break;
+        }
+        too_many_words = 1;
+        break;
       }
     }
   }
+
   if (word_too_long || too_many_words) {
     do {
       input = uart_rx(1);
@@ -205,7 +217,7 @@ void read_line(char buffer[], unsigned int word_count, unsigned int word_length)
     }
     for (i = 0; i < word_count; i++) {
       for (j = 0; j < word_length; j++) {
-	buffer[i*word_length + j] = 0x00;
+          buffer[i*word_length + j] = 0x00;
       }
     }
   }
